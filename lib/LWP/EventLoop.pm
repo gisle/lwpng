6d306dab0dadd77539ced47b25f95b75f72a91ae
Gisle Aas <gisle@aas.no>
1997-11-04 08:29:17 +0000

    one_event() maintains a queue of pending events, and will never handle more than one event any more.  Added forget_all() method.

--- lib/LWP/EventLoop.pm
+++ lib/LWP/EventLoop.pm
@@ -1,9 +1,16 @@
 package LWP::EventLoop;
 
+# $Id$
+
+# Copyright 1997 Gisle Aas.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+
+
 use strict;
 
-use vars qw($VERBOSE);
-$VERBOSE=1;
+use vars qw($DEBUG);
 
 sub new
 {
@@ -62,12 +69,21 @@ sub forget
     $self->_vec("_e", 3);
 }
 
+sub forget_all
+{
+    my $self = shift;
+    $self->{fh} = {};
+    for ("_r", "_w", "_e") {
+	$self->{$_} = undef;
+    }
+}
+
 sub _vec
 {
     my($self, $cachebits, $col) = @_;
     my $vec = "";
     for (values %{$self->{fh}}) {
-	vec($vec, fileno($_->[0]), 1) = defined($_->[$col]) ? 1 : 0;
+	vec($vec, fileno($_->[0]), 1) = 1 if defined $_->[$col];
     }
     $self->{$cachebits} = $vec;
 }
@@ -87,7 +103,7 @@ sub dump
     print "$self\n";
     my $now = time();
     for (values %{$self->{fh}}) {
-	my($fh,$r,$w,$e,$timeout) = @$_;
+	my($fh,$r,$w,$e,$timeout, $pending) = @$_;
 	printf "  %-17s %2d", ref($fh), fileno($fh);
 	for ($r, $w, $e) {
 	    if (defined) {
@@ -104,6 +120,9 @@ sub dump
 	    my @t = @$timeout;
 	    $t[2] = $now - $t[2];
 	    print "[@t]";
+	}
+	if ($pending) {
+	    print "<@$pending>";
 	}
 	print "\n";
     }
@@ -125,24 +144,33 @@ sub empty
     !$e;
 }
 
-sub one_event
+sub one_event   # or none
 {
     my $self = shift;
     my $now = time;
     my $timeout = 60;
     for (values %{$self->{fh}}) {
 	my $timeout_spec = $_->[4];
+	my $pending = $_->[5];
+
+	if ($pending && @$pending) {
+	    my $cb = shift @$pending;
+	    $self->_callback($_->[0], $cb);
+	    $timeout_spec->[2] = $now if $timeout_spec;
+	    return;
+	}
+
 	next unless $timeout_spec;
 	my $timeout_sec = $timeout_spec->[0] - ($now - $timeout_spec->[2]);
 	if ($timeout_sec <= 0) {
-	    # timeout time is now
+	    # timeout time is now, just do it
 	    $self->_callback($_->[0], $timeout_spec->[1]);
 	    $timeout_spec->[2] = $now;  # record activity
 	    return;
 	}
 	$timeout = $timeout_sec if $timeout_sec < $timeout;
     }
-    if ($VERBOSE) {
+    if ($DEBUG) {
 	print STDERR "select(";
 	print STDERR join(", ",
 			  map {
@@ -156,13 +184,14 @@ sub one_event
 	}
 	print STDERR ") = ";
     }
+
     my($r,$w,$e);
     my $nfound = select($r = $self->{_r},
 			$w = $self->{_w},
 			$e = $self->{_e},
 			$timeout);
 
-    if ($VERBOSE) {
+    if ($DEBUG) {
 	if (defined $nfound) {
 	    print STDERR "$nfound\n";
 	} else {
@@ -171,24 +200,13 @@ sub one_event
     }
 
     if ($nfound) {
+	# Add callbacks to the pending array, which will be invoked the
+	# next time one_event() runs
 	for (values %{$self->{fh}}) {
 	    my $fileno = fileno $_->[0];
-	    my $active;
-	    if (defined($r) && vec($r, $fileno, 1)) {
-		$self->_callback($_->[0], $_->[1]);
-		$active++;
-	    }
-	    if (defined($w) && vec($w, $fileno, 1)) {
-		$self->_callback($_->[0], $_->[2]);
-		$active++;
-	    }
-	    if (defined($e) && vec($e, $fileno, 1)) {
-		$self->_callback($_->[0], $_->[3]);
-		$active++;
-	    }
-	    if ($active && $_->[4]) {
-		$_->[4][2] = time();
-	    }
+	    push(@{$_->[5]}, $_->[1]) if defined($r) && vec($r, $fileno, 1);
+	    push(@{$_->[5]}, $_->[2]) if defined($w) && vec($w, $fileno, 1);
+	    push(@{$_->[5]}, $_->[3]) if defined($e) && vec($e, $fileno, 1);
 	}
     }
 }
@@ -196,7 +214,7 @@ sub one_event
 sub _callback
 {
     my($self, $fh, $cb) = @_;
-    print STDERR "_callback($fh, $cb)\n" if $VERBOSE;
+    print STDERR "_callback($fh, $cb)\n" if $DEBUG;
     my @args;
     if (ref($cb) eq "ARRAY") {
 	@args = @$cb;
@@ -209,7 +227,7 @@ sub _callback
 	    $fh->$cb(@args);
 	}
     };
-    warn $@ if $@;
+    warn $@ if $@ && $^W;
 }
 
 sub run
