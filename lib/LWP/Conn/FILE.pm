b5f7b8c42eb834c7ad8d29d478066a03fbdc3dc5
Gisle Aas <gisle@aas.no>
1998-03-19 11:54:40 +0000

    Implement ETag and various If-XXX headers.

--- lib/LWP/Conn/FILE.pm
+++ lib/LWP/Conn/FILE.pm
@@ -2,7 +2,10 @@ package LWP::Conn::FILE;
 use strict;
 require HTTP::Response;
 
-use HTTP::Date qw(time2str);
+# Ideally, we should make this implementation shareable with
+# HTTP::Daemon.
+
+use HTTP::Date qw(time2str str2time);
 use LWP::MediaTypes qw(guess_media_type);
 
 sub new
@@ -10,7 +13,7 @@ sub new
     my($class, %cnf) = @_;
     my $mgr = delete $cnf{ManagedBy} ||
       Carp::croak("'ManagedBy' is mandatory");
-    # don't care about other configuration parameters
+    # don't care about other configuration parameters yet
 
     # process all request in the queue
     while (my $req = $mgr->get_request(__PACKAGE__)) {
@@ -74,7 +77,9 @@ sub get
     local(*FILE);
     if (sysopen(FILE, $path, 0)) {
 	my $res = HTTP::Response->new(200, "OK");
-	$res->date(time);
+	my $now = time;
+
+	$res->date($now);
 	$res->server("libwww-perl");
 
 	my($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$filesize,
@@ -84,7 +89,7 @@ sub get
 	my $gname = getgrgid($gid) || $gid;
 
 	# far more than you ever wanted to know
-	$res->header("I-Node" => sprintf("[%04x]:%d", $dev, $ino)) if $ino;
+	$res->header("INode" => sprintf("[%04x]:%d", $dev, $ino)) if $ino;
 	$res->header("Owner" => $uname);
 	$res->header("Group" => $gname);
 	$res->header("Content-Length" => $filesize);
@@ -93,14 +98,91 @@ sub get
 	$res->header("Last-Accessed" => time2str($atime));
 	$res->header("Status-Modified" => time2str($ctime));
 
-	$res->header("Content-Location" => "file:$path");
+	$res->header("Content-Location" => "file:$path"); # XXX absolutize
 	guess_media_type($path, $res);
 
-	# XXX Might also implement support of headers like:
-	#    Accept
-	#    Etag
-        #    If-XXX
-	#    Range
+	# We use the same algoritm as Apache to generate an etag.
+	my $etag = sprintf qq("%x-%x-%x"), $ino, $filesize, $mtime;
+	$etag = "W/$etag" if $now - $mtime < 2;
+	$res->header("ETag" => $etag);
+
+	# Check various If-XXX headers
+	if (my $ius = $req->header("If-Unmodified-Since")) {
+	    $ius = str2time($ius);
+	    if ($ius && $mtime > $ius) {
+		$res->code(412); # PRECONDITION_FAILED
+		$res->message("Resouce modified");
+		close(FILE);
+		$req->done($res);
+		return;
+	    }
+	}
+
+	if (my @im = $req->header("If-Match")) {
+	    my $im = join(", ", @im);
+	    my $orig_im = $im;
+	    if ($im ne "*") {
+		my $match = 0;
+		while (length($im)) {
+		    if ($im =~ s|^\s*(W/)?(\"[^\"]*\")\s*,?\s*||) {
+			next if $1;  # must use strong comparison
+			if ($2 eq $etag) {
+			    $match++;
+			    last;
+			}
+		    } else {
+			last;  # illegal value
+		    }
+		}
+		#$res->header("X-Unprocessed-If-Match", $im) if $im;
+		unless ($match) {
+		    $res->code(412); # PRECONDITION_FAILED
+		    $res->message("No match for ETag $orig_im");
+		    close(FILE);
+		    $req->done($res);
+		    return;
+		}
+	    }
+	}
+
+	my $skip_if_modified;
+	if (my @inm = $req->header("If-None-Match")) {
+	    my $inm = join(", ", @inm);
+	    my $match;
+	    my $etag2 = $etag;
+	    $etag2 =~ s,^W/,,;
+	    $match = "*" if $inm eq "*";
+	    while (!$match && length($inm)) {
+		if ($inm =~ s|^\s*(W/?(\"[^\"]*\"))\s*,?\s*||) {
+		    $match = $1 if $2 eq $etag;
+		} else {
+		    last;  # illegal value
+		}
+	    }
+	    if ($match) {
+		#$res->code(412); # PRECONDITION_FAILED
+		$res->code(304); # NOT_MODIFIED
+		$res->message("ETag match for $match");
+		close(FILE);
+		$req->done($res);
+		return;
+	    }
+	    $skip_if_modified++;
+	}
+	
+	if (!$skip_if_modified &&
+	    (my $ims = $req->header("If-Modified-Since"))) {
+	    $ims = str2time($ims);
+	    if ($ims && $mtime <= $ims) {
+		$res->code(304);
+		$res->message("Not modified");
+		close(FILE);
+		$req->done($res);
+		return;
+	    }
+	}
+
+	# XXX Implement the Range header???
 
 	if ($send_content) {
 	    my $buf;
