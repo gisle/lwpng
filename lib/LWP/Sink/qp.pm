7c5ea1c21a94aa1e4ba3801ff38dab5fa9cae837
Gisle Aas <gisle@aas.no>
1998-03-27 20:28:18 +0000

    Something that actually works.

--- lib/LWP/Sink/qp.pm
+++ lib/LWP/Sink/qp.pm
@@ -3,10 +3,6 @@ package LWP::Sink::qp;
 # 'quoted-printable' is the official name of this
 # Content-Transfer-Encoding in MIME.
 
-# XXX: This is not the real implementation yet as it does not work
-# very well for arbitrary long streams.  Currently we never generate
-# output until close time.
-
 use strict;
 use vars qw(@ISA);
 
@@ -27,50 +23,46 @@ sub new
 
 sub put
 {
-    die "Must use a specific subclass";
+    my $self = shift;
+    $self->{'buf'} .= $_[0];
+    return $self if length($self->{'buf'}) < 256;
+    $self->flush;
+    $self;
+}
+
+sub flush
+{
+    my $self = shift;
+    my $sink = $self->{'sink'} || die "Missing sink";
+    my $len = rindex($self->{'buf'}, "\n") + 1;
+    return $self unless $len;
+    my $complete_lines = substr($self->{'buf'}, 0, $len);
+    substr($self->{'buf'}, 0, $len) = '';
+    $sink->put($self->eecode($complete_lines));
+    $self;
+}
+
+sub close
+{
+    my $self = shift;
+    my $sink = delete $self->{'sink'};
+    my $buf  = delete $self->{'buf'}; 
+    return unless $sink;
+    $sink->put($self->eecode($buf)) if length $buf;
+    return $sink->close;
 }
 
 package LWP::Sink::qp::encode;
 use base 'LWP::Sink::qp';
 use MIME::QuotedPrint qw(encode_qp);
 
-sub put
-{
-    my $self = shift;
-    $self->{'buf'} .= $_[0];
-    $self;
-}
-
-sub close
-{
-    my $self = shift;
-    my $sink = delete $self->{'sink'};
-    my $buf  = delete $self->{'buf'}; 
-    return unless $sink;
-    $sink->put(encode_qp($buf)) if length $buf;
-    return $sink->close;
-}
+sub eecode { encode_qp($_[1]) }
 
 
 package LWP::Sink::qp::decode;
 use base 'LWP::Sink::qp';
 use MIME::QuotedPrint qw(decode_qp);
 
-sub put
-{
-    my $self = shift;
-    $self->{'buf'} .= $_[0];
-    $self;
-}
-
-sub close
-{
-    my $self = shift;
-    my $sink = delete $self->{'sink'};
-    my $buf  = delete $self->{'buf'}; 
-    return unless $sink;
-    $sink->put(decode_qp($buf)) if length $buf;
-    return $sink->close;
-}
+sub eecode { decode_qp($_[1]) }
 
 1;
