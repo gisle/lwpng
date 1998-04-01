347ae1ef6e409aac58402807873a2a65f978fb79
Gisle Aas <gisle@aas.no>
1998-04-01 09:05:36 +0000

    Create auth objects that adds an Authorization header to the request created.

--- lib/LWP/Authen/basic.pm
+++ lib/LWP/Authen/basic.pm
@@ -37,4 +37,40 @@ sub authenticate
     return { $auth_header => $auth_value };
 }
 
+sub new
+{
+    my $class = shift;
+    my $auth;
+    my $self = bless \$auth, $class;
+    $self->login(@_) if @_;
+    $self;
+}
+
+sub set_authorization
+{
+    my($self, $req) = @_;
+    return unless defined($$self);
+    $req->header("Authorization" => $$self);
+}
+
+sub login
+{
+    my $self = shift;
+    my $old = $$self;
+    if (@_) {
+	my $user = shift;
+	unless (defined $user) {
+	    $$self = undef;
+	} else {
+	    die "Username can't contain ':'" if $user =~ /:/;
+	    my $pass = shift;
+	    $pass = "" unless defined $pass;
+	    $$self = "Basic " . MIME::Base64::encode("$user:$pass", "");
+	}
+    }
+    return unless defined $old;
+    $old =~ s/^Basic\s+//;
+    split(/:/, MIME::Base64::decode($old), 2);
+}
+
 1;
