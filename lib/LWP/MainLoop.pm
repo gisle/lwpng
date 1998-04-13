eb19ce057709c7ba6f9ac3461c472fe6a19d1e87
Gisle Aas <gisle@aas.no>
1998-04-13 20:37:13 +0000

    Added documentation.

--- lib/LWP/MainLoop.pm
+++ lib/LWP/MainLoop.pm
@@ -38,3 +38,45 @@ sub import
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+LWP::MainLoop - Give access to an single instance of LWP::EventLoop
+
+=head1 SYNOPSIS
+
+ use LWP::MainLoop qw(mainloop);
+ mainloop->readable(\*STDIN, sub {sysread(STDIN, $buf, 100)});
+ mainloop->after(10, sub { print "10 sec later"} );
+ mainloop->run;
+
+or
+
+ use LWP::MainLoop qw(readable after run);
+ readable(\*STDIN, sub {sysread(STDIN, $buf, 100)});
+ after(10, sub { print "10 sec later"} );
+ run;
+
+=head1 DESCRIPTION
+
+This module gives you access to an single instance of the
+I<LWP::EventLoop> class.  All methods of I<LWP::EventLoop> can be
+exported and used as a procedural interface.  The function mainloop()
+returns a reference to the single instance.
+
+No functions are exported by default.
+
+=head1 SEE ALSO
+
+L<LWP::EventLoop>
+
+=head1 COPYRIGHT
+
+Copyright 1997-1998, Gisle Aas
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
