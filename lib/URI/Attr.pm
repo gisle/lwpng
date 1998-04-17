ad02ce358599a397ab5098305317beb9bd0980f2
Gisle Aas <gisle@aas.no>
1998-04-17 09:13:05 +0000

    Added documentation.

--- lib/URI/Attr.pm
+++ lib/URI/Attr.pm
@@ -28,7 +28,7 @@ sub new
 }
 
 
-sub _attr
+sub _attr  # this method should probably be implemented by URI::URL itself
 {
     my($self, $url) = @_;
     $url = URI::URL->new($url) unless ref($url);
@@ -157,3 +157,131 @@ sub as_string
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+URI::Attr - associate attributes with the URI name space
+
+=head1 SYNOPSIS
+
+ use URI::Attr;
+ $attr = URI::Attr->new;
+ $attr->attr_update(SERVER => "http://www.perl.com")->{visit} = "yes";
+ if ($attr->p_attr($url, "visit")) {
+     #...
+ }
+
+=head1 DESCRIPTION
+
+Instances of the I<URI::Attr> class is able to associate attributes
+with places in the URI name space.  The main idea is to be able to
+quickly look up all attributes that are relevant to a specific
+absolute URI and to be able to override attributes at different
+hierarchal levels of the URI namespace.
+
+The levels of the URI namespace is given the following names:
+
+   GLOBAL  - affect all URIs
+   SCHEME  - affect all URIs of the given scheme
+   DOMAIN  - affect all URIs within the given domain (domains nest)
+   HOST    - a given host
+   SERVER  - a specific server (port) on the host
+   DIR     - a directory component (nestable)
+   PATH    - the final path component
+
+GLOBAL and SCHEME are the only levels available for all URIs.  The other
+levels only make sense for URIs that follow the generic URL pattern
+(like http: and ftp: schemes).  Other level names can be used for
+specific schemes.
+
+Lets take a look at an example.  Consider the following URL:
+
+   http://www.perl.com/cgi-bin/cpan_mod?module=LWP
+
+This URL can be broken up into the following hierarchal levels:
+
+   SCHEME  http
+   DOMAIN  .com
+   DOMAIN  .perl
+   HOST    www
+   SERVER  80        (implicit port)
+   DIR     cgi-bin
+   PATH    cpan-mod
+
+=head1 METHODS
+
+The following methods are provided by this class:
+
+=over 4
+
+=item $db = URI::Attr->new
+
+The constructor takes now arguments.  It returns a newly allocated
+I<URI::Attr> object.
+
+=item $db->attr($uri, [$attr_name])
+
+Look up all attributes that are relevant to the given $uri.  In scalar
+context only the most specific attribute is returned.  In list context
+all attributes are returned, with the most specific first.  Each
+attribute is represented by a reference to a 2 element array.  The
+first element is the name of the level.  The second element is the
+attribute(s).
+
+If the optional $attr_name is given, only the attribute with the given
+name is considered.  If no $attr_name is given, then the attributes
+are returned as a hash reference.
+
+=item $db->p_attr($uri, [$attr_name])
+
+Same as attr() but only return the attribute(s), not the associated
+level names.
+
+=item $db->attr_update($level, $uri)
+
+Returns a hash reference associated with $uri at the given $level.  If
+the given $level name does not make sense for the given $uri return
+<undef>.  If the $level is nestable, then the most specific instance
+related to the $uri is used.
+
+The hash returned can then be updated in order to assign attributes to
+the given place in the URI name space.
+
+=item $db->as_string
+
+Dump the content of the I<URI::Attr> object.  Mainly useful for
+debugging.
+
+=back
+
+=head1 BUGS
+
+There ought to be a way to associate attributes with domains/hosts
+without regard to scheme (or for several schemes).  Think, think,...
+
+Perhaps there should be implicit relationships between schemes, so
+that for instace everything that is valid for http is also valid for
+http.
+
+A similar concept is present in w3c-libwww under the name I<URL Tree>.
+The scheme is simply ignored here and the root of the tree is the
+hostname part of the URL.
+
+A totally different approach would be associate attributes with
+regular expressions that are matched against URLs.  Perhaps this would
+have been a better way?
+
+=head1 SEE ALSO
+
+L<URI::URL>
+
+=head1 COPYRIGHT
+
+Copyright 1998, Gisle Aas
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
